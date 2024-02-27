83460296d6912aaa0c775400e7db626a459a332b
Rajesh Chandak <rajeshgajner@gmail.com>
2024-02-27 21:03:17 +0530

    RESULTS
    *
    * *
    * * *         <-P_01

--- Patterns/Right Half Pyramid/Pattern_01.cpp
+++ Patterns/Right Half Pyramid/Pattern_01.cpp
@@ -4,18 +4,23 @@ using namespace std;
 int main()
 {
   //Inputs the size of pattern i.e Number of Rows.
+  int i = 1;
   int n;
   cout << "Enter Size :-";
   cin >> n;
 
   //i->row no.
   //j->column no.
-  
-  for(int i=1;i<=n;i++)
+  while (i <= n)
   {
-    for(int j=1;j<=i;j++)
-      cout<<i<<"  ";
-    cout<<endl;
+    int j = 1;
+    while (j <= i)
+    {
+      cout << "*" << "  ";
+      j++;
+    }
+    i++;
+    cout << endl;
   }
   return 0;
 }
