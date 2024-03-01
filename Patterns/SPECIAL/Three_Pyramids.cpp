8acef8f500253f378ca4376a7625136668c4c6d1
Rajesh Chandak <rajeshgajner@gmail.com>
2024-03-01 18:08:11 +0530

    OUTPUT
    Enter Size :-8
    
    1234567887654321
    1234567**7654321
    123456****654321
    12345******54321
    1234********4321
    123**********321
    12************21
    1**************1

---
+++ Patterns/SPECIAL/Three_Pyramids.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  //Inputs the size of pattern i.e Number of Rows.
+  int n;
+  cout << "Enter Size :-";
+  cin >> n;
+
+
+  //i->row no.
+  //j->column no.
+  for(int i=1;i<=n;i++)
+  {
+    for(int j=1;j<=n-i+1;j++)
+    {
+      cout<<j;
+    }
+    for(int j=1;j<=i*2-2;j++)
+    {
+      cout<<"*";
+    }
+    for(int j=1;j<=n-i+1;j++)
+    {
+      cout<<n-i-j+2;
+    }
+    cout<<endl;
+  
+  }
+  return 0;
+}
